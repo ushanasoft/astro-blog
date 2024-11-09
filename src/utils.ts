96a01195af0f6db1982e7bc81e8ff360191c032f
Roshan Sapkota <sapkota.ros231@gmail.com>
2024-11-09 21:21:31 +0545

    tag component

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+// Format the Date to a string.
+
 function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
@@ -7,4 +9,12 @@ function formatDate(date: Date): string {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
-export { formatDate };
+//Capitalize the first letter.
+function capitalize(str: string): string {
+  if (typeof str !== 'string' || str.length === 0) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export { formatDate, capitalize };
